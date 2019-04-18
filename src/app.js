b48d1f2e57184ac4a51639b66197d2fdf1ac2cf9
BiggaHD <dzenis.hankusic@gmail.com>
2019-04-18 05:00:01 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const getData = async url => {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+};
+
+getData('https://jsonplaceholder.typicode.com/posts');
